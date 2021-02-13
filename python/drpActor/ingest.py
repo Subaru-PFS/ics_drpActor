1a68ae50dc03cfa7e107352c5086f088269ca7e9
Craig Loomis <cloomis@astro.princeton.edu>
2021-02-12 18:56:28 -1000

    Switch to ingesting with an external process.
    Not clear that this is an improvement.

--- python/drpActor/ingest.py
+++ python/drpActor/ingest.py
@@ -1,13 +1,11 @@
-from lsst.obs.pfs.ingest import PfsIngestTask
+import logging
+import os
+import time
 
-
-def doIngest(filepath, target):
-    """example code from Craig"""
-    ingestTask = PfsIngestTask()
-    config = ingestTask.ConfigClass()
-    parser = ingestTask.ArgumentParser(name="ingest", )
-    args = parser.parse_args(config,
-                             args=[target, '--pfsConfigDir', '/data/drp/pfsDesign', '--mode=link', filepath, '-c',
-                                   'clobber=True'])
-    task = PfsIngestTask(config=args.config)
-    task.run(args)
+def doIngest(filepath, target, designDir='/data/drp/pfsDesign'):
+    logger = logging.getLogger('ingest')
+    cmd = f'ingestPfsImages.py {target} --pfsConfigDir {designDir} --config clobber=True register.ignore=True --mode=link {filepath}'
+    t0 = time.time()
+    os.system(cmd)
+    t1 = time.time()
+    logger.info(f'ingest took {t1-t0:0.4f}s: {cmd}')
