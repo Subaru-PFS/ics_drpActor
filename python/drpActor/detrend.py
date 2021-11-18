d88396a33cf0129268962d10b5d15d47c413ff14
Arnaud Le Fur <arnaud.lefur@lam.fr>
2021-11-17 19:07:04 -1000

    temporary remove cosmicRayRemoval

--- python/drpActor/detrend.py
+++ python/drpActor/detrend.py
@@ -14,6 +14,7 @@ def doDetrend(target, CALIB, rerun, visit):
 
     cmd = f"detrend.py {target} -j 2 --calib  {target}/{CALIB} --rerun {rerun} --id visit={visit} --no-versions "
     isr = ' '.join(['-c'] + [f'isr.{key}={str(val)}' for key, val in config.items()])
+    isr += ' repair.doCosmicRay=False'
     cmd += isr
     t0 = time.time()
     os.system(cmd)
