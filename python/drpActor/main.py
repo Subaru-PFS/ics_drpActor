4d049eb3a10bb0015f69a432a536d21d442ecf44
Arnaud Le Fur <arnaud.lefur@lam.fr>
2021-11-18 18:15:58 -1000

    fiddling around with ingest

--- python/drpActor/main.py
+++ python/drpActor/main.py
@@ -25,12 +25,34 @@ _monkeyPatchIncremental()
 
 import os
 import threading
-from functools import partial
 
 from actorcore.Actor import Actor
-from drpActor.utils import getInfo
-from pfs.utils.spectroIds import getSite
+from ics.utils.sps.spectroIds import getSite, SpectroIds
 from twisted.internet import reactor
+
+
+class CCDFile(object):
+    fromArmNum = dict([(v, k) for k, v in SpectroIds.validArms.items()])
+
+    def __init__(self, root, night, filename):
+        self.root = root
+        self.night = night
+        self.filename = filename
+        self.visit = int(filename[4:10])
+        self.specNum = int(filename[10])
+        self.armNum = int(filename[11])
+        self.arm = CCDFile.fromArmNum[self.armNum]
+
+        self.ingested = False
+        self.detrended = False
+
+    @property
+    def filepath(self):
+        return os.path.join(self.root, self.night, 'sps', self.filename)
+
+    @property
+    def starPath(self):
+        return os.path.join(self.root, self.night, 'sps', f'{self.filename[:10]}*.fits')
 
 
 class DrpActor(Actor):
@@ -48,6 +70,7 @@ class DrpActor(Actor):
                        configFile=configFile, modelNames=['ccd_%s' % cam for cam in self.cams])
 
         self.everConnected = False
+        self.exposureBuffer = []
 
     def connectionMade(self):
         """Called when the actor connection has been established: wire in callbacks."""
@@ -57,6 +80,8 @@ class DrpActor(Actor):
 
             for cam in self.cams:
                 self.models['ccd_%s' % cam].keyVarDict['filepath'].addCallback(self.newFilepath, callNow=False)
+
+            self.models['sps'].keyVarDict['fileIds'].addCallback(self.spsFileIds, callNow=False)
             self.everConnected = True
 
     def newFilepath(self, keyvar):
@@ -67,11 +92,32 @@ class DrpActor(Actor):
 
         self.logger.info(f'newfilepath: {root}, {night}, {fname}. threads={threading.active_count()}')
 
-        filepath = os.path.join(root, night, 'sps', fname)
-        # self.callCommand('process filepath=%s' % filepath)
-        self.callCommand('ingest filepath=%s' % filepath)
-        visit, arm = getInfo(filepath)
-        reactor.callLater(5, partial(self.callCommand, f'detrend visit={visit} arm={arm}'))
+        self.exposureBuffer.append(CCDFile(root, night, fname))
+        reactor.callLater(5, self.checkLeftOver)
+
+    def checkLeftOver(self):
+        pass
+
+    def spsFileIds(self, keyvar):
+        # reactor.callLater(5, partial(self.callCommand, f'detrend visit={visit} arm={arm}'))
+
+        try:
+            [visit, camList, camMask] = keyvar.getValue()
+        except ValueError:
+            return
+
+        # get exposure with same visit
+        files = [file for file in self.exposureBuffer if file.visit == visit]
+        if not files:
+            return
+
+        nights = list(set([file.night for file in files]))
+        for night in nights:
+            filesPerNight = [file for file in files if file.night == night]
+            for file in filesPerNight:
+                file.ingested = True
+            startPath = files[0].starPath
+            self.callCommand('ingest filepath=%s' % startPath)
 
 
 def main():
