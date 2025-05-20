e1d369e60cd96bd8b6c6b38f9e3ddc1423882606
Arnaud Le Fur <afur@princeton.astro.edu>
2025-05-19 22:13:00 -1000

    more logging

--- python/drpActor/utils/extractFlux.py
+++ python/drpActor/utils/extractFlux.py
@@ -39,26 +39,32 @@ def getWindowedFluxes(exp, dataId, fiberTrace, detectorMap, darkVariance=30, **k
     dataId = dataId.copy()
     dataId.update(kwargs)
 
-    md = exp.getMetadata()
-    row0, row1 = md["W_CDROW0"], md["W_CDROWN"]
+    try:
+        md = exp.getMetadata()
+        row0, row1 = md["W_CDROW0"], md["W_CDROWN"]
 
-    maskVal = exp.mask.getPlaneBitMask(["SAT", "NO_DATA"])
-    exp.mask.array[0:row0] = maskVal
-    exp.mask.array[row1 + 1:] = maskVal
-    exp.image.array[exp.mask.array != 0] = np.nan  # not 0; we're going to use np.nanmedian later
+        maskVal = exp.mask.getPlaneBitMask(["SAT", "NO_DATA"])
+        exp.mask.array[0:row0] = maskVal
+        exp.mask.array[row1 + 1:] = maskVal
+        exp.image.array[exp.mask.array != 0] = np.nan  # not 0; we're going to use np.nanmedian later
 
-    for amp in exp.getDetector():
-        exp[amp.getRawBBox()].image.array -= np.nanmedian(exp[amp.getRawHorizontalOverscanBBox()].image.array)
+        for amp in exp.getDetector():
+            exp[amp.getRawBBox()].image.array -= np.nanmedian(
+                exp[amp.getRawHorizontalOverscanBBox()].image.array
+            )
 
-    exp = assembleTask.assembleCcd(exp)
-    exp.variance = exp.image
-    exp.variance.array += darkVariance  # need a floor to the noise to reduce the b arm
+        exp = assembleTask.assembleCcd(exp)
+        exp.variance = exp.image
+        exp.variance.array += darkVariance
 
-    spectra = extractSpectra.run(exp.maskedImage, fiberTrace, detectorMap).spectra.toPfsArm(dataId)
-    spectra.flux[spectra.mask != 0] = np.nan
+        spectra = extractSpectra.run(exp.maskedImage, fiberTrace, detectorMap).spectra.toPfsArm(dataId)
+        spectra.flux[spectra.mask != 0] = np.nan
 
-    df = pd.DataFrame(dict(flux=np.nanmedian(spectra.flux, axis=1), fiberId=spectra.fiberId))
-    # calculating time.
-    totalTime = round(time.time() - start, 1)
-    logging.info(f'{dataId} flux extracted in {totalTime}s')
+        df = pd.DataFrame(dict(flux=np.nanmedian(spectra.flux, axis=1), fiberId=spectra.fiberId))
+        logging.info(f'{dataId} flux extracted in {round(time.time() - start, 1)}s')
+
+    except Exception as e:
+        logging.warning(f'Failed to extract flux for {dataId}: {type(e).__name__}: {e}')
+        df = pd.DataFrame(columns=["flux", "fiberId"])
+
     return df
