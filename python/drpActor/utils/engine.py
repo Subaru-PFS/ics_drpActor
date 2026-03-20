c0070a2be9d4a1d70bcb8acabdec91fbffa49691
Arnaud Le Fur <afur@princeton.astro.edu>
2026-03-20 17:12:13 -0400

    rewrite dotRoach flux extraction for new MCS-based convergence sequence
    DotRoach now extracts windowed flat flux per cobra, normalizes by lamp
    response using BROKENCOBRA fibers as monitors, and inserts results into
    the new dot_roach_flux opdb table. startDotRoach/processDotRoach/
    stopDotRoach commands drive the loop from iic. Calibrations (fiberTrace,
    detectorMap) are built once on the first exposure and cached per camera.
    Flux extraction runs in parallel subprocesses per camera.

--- python/drpActor/utils/engine.py
+++ python/drpActor/utils/engine.py
@@ -17,6 +17,7 @@ from lsst.ctrl.mpexec import SeparablePipelineExecutor
 from lsst.pipe.base import Pipeline, ExecutionResources
 from drpActor.utils.chainedCollection import extend_collection_chain
 from ics.utils.opdb import opDB
+from pfs.utils.database.opdb import OpDB
 
 
 class DrpEngine:
@@ -102,6 +103,7 @@ class DrpEngine:
         self.rawButler = self.loadButler(self.rawRun)
         self.pfsConfigButler = self.loadButler(self.pfsConfigRun)
         self.butler = Butler(self.datastore, collections=[self.inputCollection, self.outputCollection])
+        self.opdb = OpDB()
 
         self.ingestHandler = IngestHandler(self)
         self.reducePipeline, self.reduceButler, self.executor, self.timestamp = self.setupReducePipeline(datastore,
@@ -149,6 +151,7 @@ class DrpEngine:
         # opdb
         opdbHost = siteConfig.get('opdb').get('host')
         opDB.host = opdbHost
+        OpDB.set_default_connection(host=opdbHost)
 
         # logs and callbacks
         lsstLog = siteConfig.get('lsstLog')
@@ -492,16 +495,13 @@ class DrpEngine:
 
             self.configOverride = configOverride
 
-    def startDotRoach(self, dataRoot, maskFile, cams, keepMoving=False):
+    def startDotRoach(self, cams):
         """Starting dotRoach loop."""
-        # Deactivating auto-detrend.
         self.doAutoReduce = False
-        # instantiating DotRoach object.
-        self.dotRoach = dotRoach.DotRoach(self, dataRoot, maskFile, cams, keepMoving=keepMoving)
+        self.dotRoach = dotRoach.DotRoach(self, cams)
 
     def stopDotRoach(self, cmd):
-        """Stopping dotRoach loop"""
-        # Re-activating auto-detrend.
+        """Stopping dotRoach loop."""
         self.doAutoReduce = True
 
         if not self.dotRoach:
@@ -509,5 +509,4 @@ class DrpEngine:
             return
 
         cmd.inform('text="ending dotRoach loop"')
-        self.dotRoach.finish()
         self.dotRoach = None
--- python/drpActor/utils/extractFlux.py
+++ python/drpActor/utils/extractFlux.py
@@ -4,6 +4,7 @@ import time
 import numpy as np
 import pandas as pd
 from lsst.ip.isr import AssembleCcdTask
+from pfs.utils.fiberids import FiberIds
 
 config = AssembleCcdTask.ConfigClass()
 config.doTrim = True
@@ -16,14 +17,41 @@ config.doCrosstalk = False
 config.validate()
 extractSpectra = ExtractSpectraTask(config=config)
 
+gfm = FiberIds()
+
+
+def applyQuickISR(exp, darkVariance=30):
+    """Overscan-subtract per amp and assemble CCD (no windowing).
+
+    Parameters
+    ----------
+    exp : `lsst.afw.image.Exposure`
+        Raw float exposure (call convertF() before passing in).
+    darkVariance : `float`, optional
+        Variance floor added after assembly to suppress read-noise artefacts.
+
+    Returns
+    -------
+    exp : `lsst.afw.image.Exposure`
+        Assembled, overscan-subtracted exposure ready for trace centroiding
+        or spectrum extraction.
+    """
+    for amp in exp.getDetector():
+        exp[amp.getRawBBox()].image.array -= np.nanmedian(exp[amp.getRawHorizontalOverscanBBox()].image.array)
+
+    exp = assembleTask.assembleCcd(exp)
+    exp.variance = exp.image
+    exp.variance.array += darkVariance
+    return exp
+
 
 def getWindowedFluxes(exp, dataId, fiberTrace, detectorMap, darkVariance=30, **kwargs):
     """Return an estimate of the median flux in each fibre
 
      Parameters
     ----------
-    butler : `lsst.daf.butler.Butler`
-        Butler instance for retrieving datasets.
+    exp : `lsst.afw.image.Exposure`
+        Raw float exposure (call convertF() before passing in).
     dataId : `dict`
         Dictionary specifying the exposure to process (e.g., exposure ID).
     fiberTrace : `FiberTrace`
@@ -47,18 +75,16 @@ def getWindowedFluxes(exp, dataId, fiberTrace, detectorMap, darkVariance=30, **k
     exp.mask.array[row1 + 1:] = maskVal
     exp.image.array[exp.mask.array != 0] = np.nan  # not 0; we're going to use np.nanmedian later
 
-    for amp in exp.getDetector():
-        exp[amp.getRawBBox()].image.array -= np.nanmedian(exp[amp.getRawHorizontalOverscanBBox()].image.array)
-
-    exp = assembleTask.assembleCcd(exp)
-    exp.variance = exp.image
-    exp.variance.array += darkVariance  # need a floor to the noise to reduce the b arm
+    exp = applyQuickISR(exp, darkVariance=darkVariance)
 
     spectra = extractSpectra.run(exp.maskedImage, fiberTrace, detectorMap).spectra.toPfsArm(dataId)
     spectra.flux[spectra.mask != 0] = np.nan
 
-    df = pd.DataFrame(dict(flux=np.nanmedian(spectra.flux, axis=1), fiberId=spectra.fiberId))
+    df = pd.DataFrame(dict(flux=np.nanmedian(spectra.flux, axis=1), fiberId=spectra.fiberId,
+                           cobraId=gfm.fiberIdToCobraId(spectra.fiberId)))
+
     # calculating time.
     totalTime = round(time.time() - start, 1)
     logging.info(f'{dataId} flux extracted in {totalTime}s')
+
     return df
